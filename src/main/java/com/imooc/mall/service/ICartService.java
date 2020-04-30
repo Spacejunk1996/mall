2712f0f4f2fd27b478005fa986907fa397770b3f
Zixu Jiang <zixu.jiang1996@gmail.com>
2020-04-30 17:14:54 -0400

    完成购物车全选/不选/总数开发

--- src/main/java/com/imooc/mall/service/ICartService.java
+++ src/main/java/com/imooc/mall/service/ICartService.java
@@ -2,8 +2,11 @@ package com.imooc.mall.service;
 
 import com.imooc.mall.form.CartAddForm;
 import com.imooc.mall.form.CartUpdateForm;
+import com.imooc.mall.pojo.Cart;
 import com.imooc.mall.vo.CartVo;
 import com.imooc.mall.vo.ResponseVo;
+
+import java.util.List;
 
 /**
  * @author Zixu Jiang
@@ -18,4 +21,12 @@ public interface ICartService {
     ResponseVo<CartVo> update(Integer uid, Integer productId, CartUpdateForm form);
 
     ResponseVo<CartVo> delete(Integer uid, Integer productId);
+
+    ResponseVo<CartVo> selectAll(Integer uid);
+
+    ResponseVo<CartVo> unSelectAll(Integer uid);
+
+    ResponseVo<Integer> sum(Integer uid);
+
+    List<Cart> listForCart(Integer uid);
 }
--- src/test/java/com/imooc/mall/service/impl/CartServiceImplTest.java
+++ src/test/java/com/imooc/mall/service/impl/CartServiceImplTest.java
@@ -2,12 +2,16 @@ package com.imooc.mall.service.impl;
 
 import com.google.gson.Gson;
 import com.imooc.mall.MallApplicationTests;
+import com.imooc.mall.enums.ResponseEnum;
 import com.imooc.mall.form.CartAddForm;
 import com.imooc.mall.form.CartUpdateForm;
 import com.imooc.mall.service.ICartService;
 import com.imooc.mall.vo.CartVo;
 import com.imooc.mall.vo.ResponseVo;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -24,18 +28,25 @@ public class CartServiceImplTest extends MallApplicationTests {
     @Autowired
     private Gson gson;
 
-    @Test
+    private Integer uid = 1;
+
+    private Integer productId = 26;
+
+    @Before
     public void add() {
         CartAddForm form = new CartAddForm();
-        form.setProductId(26);
+        form.setProductId(productId);
         form.setSelected(true);
-        cartService.add(1, form);
+        ResponseVo<CartVo> responseVo = cartService.add(uid, form);
+        log.info("list={}", gson.toJson(responseVo));
+        Assert.assertEquals(ResponseEnum.SUCCESS.getCode(), responseVo.getStatus());
     }
 
     @Test
     public void list() {
-        ResponseVo<CartVo> list = cartService.list(1);
-        log.info("list={}", list);
+        ResponseVo<CartVo> responseVo = cartService.list(uid);
+        log.info("list={}", responseVo);
+        Assert.assertEquals(ResponseEnum.SUCCESS.getCode(), responseVo.getStatus());
     }
 
     @Test
@@ -43,13 +54,29 @@ public class CartServiceImplTest extends MallApplicationTests {
         CartUpdateForm form = new CartUpdateForm();
         form.setQuantity(5);
         form.setSelected(false);
-        ResponseVo<CartVo> responseVo = cartService.update(1,26, form);
+        ResponseVo<CartVo> responseVo = cartService.update(uid,productId, form);
         log.info("list={}", gson.toJson(responseVo));
+        Assert.assertEquals(ResponseEnum.SUCCESS.getCode(), responseVo.getStatus());
+    }
+
+    @After
+    public void delete() {
+        ResponseVo<CartVo> responseVo = cartService.delete(uid,productId);
+        log.info("list={}", gson.toJson(responseVo));
+        Assert.assertEquals(ResponseEnum.SUCCESS.getCode(), responseVo.getStatus());
     }
 
     @Test
-    public void delete() {
-        ResponseVo<CartVo> responseVo = cartService.delete(1,27);
+    public void selectAll() {
+        ResponseVo<CartVo> responseVo = cartService.selectAll(uid);
         log.info("list={}", gson.toJson(responseVo));
+        Assert.assertEquals(ResponseEnum.SUCCESS.getCode(), responseVo.getStatus());
+    }
+
+    @Test
+    public void unSelectAll() {
+        ResponseVo<CartVo> responseVo = cartService.unSelectAll(uid);
+        log.info("list={}", gson.toJson(responseVo));
+        Assert.assertEquals(ResponseEnum.SUCCESS.getCode(), responseVo.getStatus());
     }
 }
